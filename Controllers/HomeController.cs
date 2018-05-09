8de3d1f302196a908536f7a552c378379846dffc
Vince DeGiorgio <vincedg74@gmail.com>
2018-05-09 18:14:06 -0400

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This is about me! Hello World";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            var profile = new Models.Profile();
+            ViewBag.Title = "Welcome To Business Technology";
+            ViewBag.Message = "This is my contact page.";
+            return View(profile);
+        }
+    }
+}
---
+++ Models/Profile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Profile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public Profile()
+        {
+            FirstName = "Vince";
+            LastName = "DeGiorgio";
+            Email = "vincent.degiorgio@aero.bombardier.com";
+        }
+    }
+}
